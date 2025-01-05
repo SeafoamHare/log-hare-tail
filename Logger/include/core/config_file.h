6cd0bf166a717fed53bfb9d334d78e314e8b5c3a
SeafoamHare <130981975+SeafoamHare@users.noreply.github.com>
2025-01-05 10:40:26 +0000

    MODFIY:: CODING STYLE

--- Logger/include/core/config_file.h
+++ Logger/include/core/config_file.h
@@ -14,19 +14,19 @@ using json = nlohmann::json;
 class Config_File
 {
 public:
-    // 構造函數，從配置檔案讀取配置
-    explicit Config_File(const std::string &config_file);
+  // 構造函數，從配置檔案讀取配置
+  explicit Config_File(const std::string &config_file);
 
-    // 配置文件相關屬性
-    std::string filename;      // 日誌文件名
-    size_t max_size;           // 日誌文件的最大大小
-    int max_files;             // 最多保留的日誌文件數量
-    std::string app_name;      // 應用程序名稱
-    std::string log_directory; // 日誌存放目錄
+  // 配置文件相關屬性
+  std::string filename;      // 日誌文件名
+  size_t max_size;           // 日誌文件的最大大小
+  int max_files;             // 最多保留的日誌文件數量
+  std::string app_name;      // 應用程序名稱
+  std::string log_directory; // 日誌存放目錄
 
 private:
-    // 創建目錄，並確保父目錄也存在
-    bool createDirectory(const std::string &path);
+  // 創建目錄，並確保父目錄也存在
+  bool createDirectory(const std::string &path);
 };
 
 #endif // CONFIG_FILE_H
--- Logger/src/core/LogLevel.cxx
+++ Logger/src/core/LogLevel.cxx
@@ -4,17 +4,17 @@ std::string loglevelToString(LogLevel level)
 {
     switch (level)
     {
-        case LogLevel::DEBUG:
-            return "DEBUG";
-        case LogLevel::INFO:
-            return "INFO";
-        case LogLevel::WARN:
-            return "WARN";
-        case LogLevel::ERROR:
-            return "ERROR";
-        case LogLevel::CRITICAL:
-            return "CRITICAL";
-        default:
-            return "UNKNOWN";
+    case LogLevel::DEBUG:
+        return "DEBUG";
+    case LogLevel::INFO:
+        return "INFO";
+    case LogLevel::WARN:
+        return "WARN";
+    case LogLevel::ERROR:
+        return "ERROR";
+    case LogLevel::CRITICAL:
+        return "CRITICAL";
+    default:
+        return "UNKNOWN";
     }
 }
